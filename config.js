798c5490801e949d2014aebd07c1b73e9474b431
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-01-21 08:00:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const global = {
-    owner: [6142885267], // ganti jadi id mu
-    botToken: "7725260979:AAF9pls5ef2mKp0eZu-djXeFnnwC5XCRyL8", //isi make token bot mu
+    owner: [7430156268], // ganti jadi id mu
+    botToken: "7776286968:AAEv-aNlU21h8E2uEiHiAsj8mY7PEVqA2ks", //isi make token bot mu
 }
 
 module.exports = global;
